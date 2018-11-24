66fcbcb8b0f8b81f696074431048893255b7fae6
DESKTOP-BJIVU7J\Asus-PC <srihhandayanii@gmail.com>
2018-11-25 07:42:05 +0800

    update

--- app/src/main/java/politanismd/freshlink/model/ResponseUser.java
+++ app/src/main/java/politanismd/freshlink/model/ResponseUser.java
@@ -7,9 +7,45 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 
 public class ResponseUser implements Serializable {
-    @SerializedName("user")
+    @SerializedName("success")
+    @Expose
+    public Boolean success=false;
+    
+    @SerializedName("data")
     @Expose
     private User user;
+
+    @SerializedName("message")
+    @Expose
+    public String message;
+
+    @SerializedName("error")
+    @Expose
+    public String error;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
 
     public User getUser() {
         return user;
