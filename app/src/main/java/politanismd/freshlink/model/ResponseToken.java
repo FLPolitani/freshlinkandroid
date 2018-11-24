b04671e76f8e5bf780c955b591fd754939f347e3
DESKTOP-BJIVU7J\Asus-PC <srihhandayanii@gmail.com>
2018-11-25 07:10:44 +0800

    update

--- app/src/main/java/politanismd/freshlink/model/ResponseToken.java
+++ app/src/main/java/politanismd/freshlink/model/ResponseToken.java
@@ -1,4 +1,20 @@
 package politanismd.freshlink.model;
 
-public class ResponseToken {
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class ResponseToken implements Serializable {
+    @SerializedName("token")
+    @Expose
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
--- app/src/main/java/politanismd/freshlink/service/ApiEndpointService.java
+++ app/src/main/java/politanismd/freshlink/service/ApiEndpointService.java
@@ -1,8 +1,18 @@
 package politanismd.freshlink.service;
 
+import politanismd.freshlink.model.ResponseToken;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 /**
  * Created by Eko on 12/07/2017.
  */
 
 public interface ApiEndpointService {
+    @FormUrlEncoded
+    @POST("token")
+    Call<ResponseToken> getAccessToken(@Field("email") String email, @Field("password") String password);
+
 }
