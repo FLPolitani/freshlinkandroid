5e37e10fdb25b6bfdcbf71264be701723487d57c
DESKTOP-BJIVU7J\Asus-PC <srihhandayanii@gmail.com>
2018-11-25 06:21:13 +0800

    model user

--- app/src/main/java/politanismd/freshlink/model/ResponseUser.java
+++ app/src/main/java/politanismd/freshlink/model/ResponseUser.java
@@ -1,10 +1,23 @@
 package politanismd.freshlink.model;
 
-<<<<<<< HEAD
-public class ResponseUser {
-=======
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class ResponseUser implements Serializable {
->>>>>>> origin/master
+    @SerializedName("user")
+    @Expose
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
 }
--- app/src/main/java/politanismd/freshlink/model/User.java
+++ app/src/main/java/politanismd/freshlink/model/User.java
@@ -1,6 +1,109 @@
 package politanismd.freshlink.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 public class User implements Serializable {
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("deleted_at")
+    @Expose
+    private Object deletedAt;
+    @SerializedName("verified")
+    @Expose
+    private Integer verified;
+    @SerializedName("verification_token")
+    @Expose
+    private Object verificationToken;
+    @SerializedName("device_token")
+    @Expose
+    private Object deviceToken;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Object getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Object deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public Integer getVerified() {
+        return verified;
+    }
+
+    public void setVerified(Integer verified) {
+        this.verified = verified;
+    }
+
+    public Object getVerificationToken() {
+        return verificationToken;
+    }
+
+    public void setVerificationToken(Object verificationToken) {
+        this.verificationToken = verificationToken;
+    }
+
+    public Object getDeviceToken() {
+        return deviceToken;
+    }
+
+    public void setDeviceToken(Object deviceToken) {
+        this.deviceToken = deviceToken;
+    }
+
 }
